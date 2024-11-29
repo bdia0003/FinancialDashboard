ad1bbd276dbe3ad06898fafabe27b0ec1c7e1d43
Brent <54158491+bdia0003@users.noreply.github.com>
2024-11-29 19:55:06 +1100

    Updates

---
+++ static/js/952.c274faf5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkhorizon_ui_chakra=self.webpackChunkhorizon_ui_chakra||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.c274faf5.chunk.js.map
